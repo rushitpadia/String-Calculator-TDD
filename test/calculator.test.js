98cd31764323d41e47fad6123a944e864731ce18
Rushit Padia <rushit.padia@greylabs.com>
2025-09-25 23:04:00 +0530

    test : Testcase 3: should return sum of two numbers separated by comma

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -9,4 +9,7 @@ describe("String Calculator TDD", () => {
     it("Testcase 2: should return the number itself if only one number is provided", () => {
         expect(add("4")).to.equal(4);
     });
+    it("Testcase 3: should return sum of two numbers separated by comma", () => {
+        expect(add("4,5")).to.equal(9);
+    });
 });
