cd5e36e8cc977c1fcddf23fa28b94d9533d20488
Rushit Padia <rushit.padia@greylabs.com>
2025-09-26 00:09:56 +0530

    converted function to class to have ability to store count

--- src/calculator.js
+++ src/calculator.js
@@ -1,21 +1,23 @@
-function add(stringNumbers){
-    if(stringNumbers === null || stringNumbers === "") return 0;
-    let delimiterPattern = /[\n,]/;
-    let numbers = stringNumbers;
-    // Handle custom delimiters
-    if(numbers.startsWith("//")){
-        const delimiterLineEndIndex = numbers.indexOf("\n"); //first \n position will be returned.
-        const delimiter = numbers.substring(2, delimiterLineEndIndex);
-        delimiterPattern = new RegExp(`[${delimiter}\n,]`);
-        numbers = numbers.substring(delimiterLineEndIndex + 1);
-    }
-    const splitStringNumbers = numbers.split(delimiterPattern).map(Number);
-
-    // checks for negative numbers
-    const negativeNumbers = splitStringNumbers.filter(num => num < 0); 
-    if(negativeNumbers.length > 0) throw new Error(`Negatives not allowed: ${negativeNumbers.join(",")}`)
+class StringCalculatorTDD {
+    add(stringNumbers){
+        if(stringNumbers === null || stringNumbers === "") return 0;
+        let delimiterPattern = /[\n,]/;
+        let numbers = stringNumbers;
+        // Handle custom delimiters
+        if(numbers.startsWith("//")){
+            const delimiterLineEndIndex = numbers.indexOf("\n"); //first \n position will be returned.
+            const delimiter = numbers.substring(2, delimiterLineEndIndex);
+            delimiterPattern = new RegExp(`[${delimiter}\n,]`);
+            numbers = numbers.substring(delimiterLineEndIndex + 1);
+        }
+        const splitStringNumbers = numbers.split(delimiterPattern).map(Number);
     
-    return splitStringNumbers.reduce((sum, num) => sum + num,0);
+        // checks for negative numbers
+        const negativeNumbers = splitStringNumbers.filter(num => num < 0); 
+        if(negativeNumbers.length > 0) throw new Error(`Negatives not allowed: ${negativeNumbers.join(",")}`)
+        
+        return splitStringNumbers.reduce((sum, num) => sum + num,0);
+    }
 }
 
-module.exports = { add };
+module.exports = StringCalculatorTDD;
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,29 +1,31 @@
 const { expect } = require("chai");
-const { add }  = require('../src/calculator');
+const StringCalculatorTDD  = require('../src/calculator');
 const { describe } = require("mocha");
 
 describe("String Calculator TDD", () => {
+    let calculator;
+    beforeEach(() => calculator = new StringCalculatorTDD());
     it("Testcase 1: Should return 0 for an empty string", () => {
-        expect(add("")).to.equal(0);
+        expect(calculator.add("")).to.equal(0);
     });
     it("Testcase 2: should return the number itself if only one number is provided", () => {
-        expect(add("4")).to.equal(4);
+        expect(calculator.add("4")).to.equal(4);
     });
     it("Testcase 3: should return sum of two numbers separated by comma", () => {
-        expect(add("4,5")).to.equal(9);
+        expect(calculator.add("4,5")).to.equal(9);
     });
     it("Testcase 4: should return sum of multiple comma-separated numbers", () => {
-        expect(add("1,2,3,4,5")).to.equal(15);
+        expect(calculator.add("1,2,3,4,5")).to.equal(15);
     });
     it("Testcase 5: should handle new lines between numbers as well as commas", () => {
-        expect(add("1\n2,3")).to.equal(6);
+        expect(calculator.add("1\n2,3")).to.equal(6);
     })
     it("Testcase 6: should support custom delimiters", () => {
-        expect(add("//;\n1;2")).to.equal(3);
-        expect(add("//|\n2|3|4")).to.equal(9);
+        expect(calculator.add("//;\n1;2")).to.equal(3);
+        expect(calculator.add("//|\n2|3|4")).to.equal(9);
     });
     it("Testcase 7: should throw an exception when a negative number is provided", () => {
-        expect(() => add("1,-2,3,-4")).to.throw("Negatives not allowed: -2,-4");
-        expect(() => add("-5")).to.throw("Negatives not allowed: -5");
+        expect(() => calculator.add("1,-2,3,-4")).to.throw("Negatives not allowed: -2,-4");
+        expect(() => calculator.add("-5")).to.throw("Negatives not allowed: -5");
     });
 });
