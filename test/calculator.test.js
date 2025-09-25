1589a24b87215939e8d2385effbf93cf8192aa0f
Rushit Padia <rushit.padia@greylabs.com>
2025-09-25 23:18:38 +0530

    test: Testcase 5: should handle new lines between numbers as well as commas

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -15,4 +15,7 @@ describe("String Calculator TDD", () => {
     it("Testcase 4: should return sum of multiple comma-separated numbers", () => {
         expect(add("1,2,3,4,5")).to.equal(15);
     });
+    it("Testcase 5: should handle new lines between numbers as well as commas", () => {
+        expect(add("1\n2,3")).to.equal(6);
+      })
 });
