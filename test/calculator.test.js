5ad95f782dba525ce7dbfe9a2d4a1d164de9d9d3
Rushit Padia <rushit.padia@greylabs.com>
2025-09-25 22:56:14 +0530

    test: Testcase 2: should return the number itself if only one number is provided

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -6,4 +6,7 @@ describe("String Calculator TDD", () => {
     it("Testcase 1: Should return 0 for an empty string", () => {
         expect(add("")).to.equal(0);
     });
+    it("Testcase 2: should return the number itself if only one number is provided", () => {
+        expect(add("4")).to.equal(4);
+    });
 });
