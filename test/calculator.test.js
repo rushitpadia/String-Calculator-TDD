deaa2324bef70d3d1581075cf719f5d315682237
Rushit Padia <rushit.padia@greylabs.com>
2025-09-25 23:27:33 +0530

    test: Testcase 6: should support custom delimiters

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -17,5 +17,9 @@ describe("String Calculator TDD", () => {
     });
     it("Testcase 5: should handle new lines between numbers as well as commas", () => {
         expect(add("1\n2,3")).to.equal(6);
-      })
+    })
+    it("Testcase 6: should support custom delimiters", () => {
+        expect(add("//;\n1;2")).to.equal(3);
+        expect(add("//|\n2|3|4")).to.equal(9);
+      });
 });
